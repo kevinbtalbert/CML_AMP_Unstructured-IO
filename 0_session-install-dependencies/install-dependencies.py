847f80cf6c29077594ac68dc6d22b357bb1aff3f
Kevin Talbert <ktalber3@uncc.edu>
2024-09-02 16:56:37 -0400

    updated requirements

--- 0_session-install-dependencies/install-dependencies.py
+++ 0_session-install-dependencies/install-dependencies.py
@@ -1,5 +1,3 @@
-## This file is provided to the user to accelerate installing dependencies. 
-## Look at sample implementations of this in practice as part of the AMP build script in sample-project-metadata/project-metadata-sample-1.yaml
+import subprocess
 
-!pip install --upgrade pip
-!pip install --no-cache-dir --log 0_session-install-dependencies/pip-req.log -r 0_session-install-dependencies/requirements.txt
+print(subprocess.run(["sh 0_session-install-dependencies/install-dependencies.sh"], shell=True))
---
+++ 0_session-install-dependencies/install-dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install --upgrade pip
+pip install --no-cache-dir --log 0_session-install-dependencies/pip-req.log -r 0_session-install-dependencies/requirements.txt
